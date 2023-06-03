8abced901446a8c45b82cc99a9d070bdd409d670
Korede Faleye <akfaleye@gmail.com>
2023-06-03 09:56:32 +0100

    food-delivery: Implement category items template with javascript

--- food-delivery/assets/script/script.js
+++ food-delivery/assets/script/script.js
@@ -1,3 +1,16 @@
 let foodCategoriesItems = ["burger", "pizza", "salad", "chicken"];
-
-// for(item of )
+const foodCategoryitemsDom = document.querySelector(".category .items");
+console.log(foodCategoryitemsDom);
+// Render each food category item 
+for (item of foodCategoriesItems) {
+  item = `
+    <button class="btn btn-secondary item ${item === 'burger' ? 'active': "" }" onclick="${item}()">
+    <span class="item-bg">
+      <img src="./assets/images/categories/${item}.png" alt="">
+    </span>
+    <p class="item-name">${item}</p>
+  </button>
+    `;
+  
+  foodCategoryitemsDom.innerHTML += item;
+}
