ff40bd17e3d01772af621f6bb9c434ff263f4ea2
Korede Faleye <akfaleye@gmail.com>
2023-06-16 14:51:07 +0100

    food-delivery: write javaScript to make #back functional

--- food-delivery/assets/script/script.js
+++ food-delivery/assets/script/script.js
@@ -137,3 +137,8 @@ function loadPopularCategory(categoryName) {
    ############################# */
 
 
+const backBtn = document.querySelector("#back");
+
+backBtn.addEventListener("click", function () {
+  window.history.back();
+})
